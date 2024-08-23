de092f06fb22ab3bd887d7fafceea7657d1e36c6
ddcsoftdev <ddcsoftwaredev@gmail.com>
2024-08-23 12:45:21 +0200

    formatting

--- eshop/apps/order/models.py
+++ eshop/apps/order/models.py
@@ -4,27 +4,13 @@ from apps.product.models import Product
 
 
 class Order(models.Model):
-    code = models.CharField(
-        max_length=100, 
-        unique=True
-    )
-    price = models.DecimalField(
-        max_digits=20, 
-        decimal_places=2
-    )
-    purchase_date = models.DateField(
-        null=True
-    )
+    code = models.CharField(max_length=100, unique=True)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    purchase_date = models.DateField(null=True)
     user = models.ForeignKey(
-        User, 
-        on_delete=models.SET_NULL, 
-        null=True, 
-        related_name="orders"
+        User, on_delete=models.SET_NULL, null=True, related_name="orders"
     )
-    products = models.ManyToManyField(
-        Product, 
-        related_name="orders"
-    )
+    products = models.ManyToManyField(Product, related_name="orders")
 
     def __str__(self):
         return self.code
